786ffe8848112a8beeb2beba367206845ac3dfac
namminimi <tbvhdaos@naver.com>
2023-11-20 23:57:11 +0900

    feat: 초기셋팅

---
+++ src/config/apiurl.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const apiurl = () => {
+    return <div></div>;
+};
+
+export default apiurl;
---
+++ src/moduls/moduls.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const moduls = () => {
+    return <div></div>;
+};
+
+export default moduls;
